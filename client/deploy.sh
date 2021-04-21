cfb3b00b3c5d2b16c98bdf5cb78247a1f0a9115c
VNuserX <vnuserx@mail.com>
2021-04-21 09:51:45 +0300

    added deployment scripts

---
+++ client/deploy.sh
@@ -0,0 +1,13 @@
+docker build -t vnuserx/multi-client:latest -t vnuserx/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t vnuserx/multi-server:latest -t vnuserx/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t vnuserx/multi-worker:latest -t vnuserx/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push vnuserx/multi-client:latest
+docker push vnuserx/multi-server:latest
+docker push vnuserx/multi-worker:latest
+docker push vnuserx/multi-client:$SHA
+docker push vnuserx/multi-server:$SHA
+docker push vnuserx/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=multi-server:$SHA
+kubectl set image deployments/client-deployment client=multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=multi-worker:$SHA
